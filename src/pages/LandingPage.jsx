0f286366a3bcde498ebcd831c12d7d5e02d25282
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-03-30 10:26:31 +0100

    link

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import Background from "../components/Background";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
     return ( 
@@ -16,7 +17,7 @@ const LandingPage = () => {
                     <p className="text-white text-[10.5px] md:text-[11px]">
                     Zemyen Studios is a creative media outfit based in Jos, Plateau state, Nigeria. We provide photography/Videography services; we are into filmmaking, documentary production, graphic designs, printing, public relations, adverts, jingles, Voice-over, creative writing services and training in these areas. The outfit started in 2015 under the name Zemyen Media before its business name registration in 2019. Emmanuel Kwapyil, a Mass communication graduate from the University of Jos is the CEO and founder of the organization. He is multi-skilled in the areas of the business operation and has worked in other organizations where he has gained valuable experiences; he was Media officer at Stefanos Foundation, an NGO, Monitoring and Evaluation Officer at Put'Em Right Reproductive Health Foundation, another NGO and an intern Journalist (correspondent) with the Nation Newspaper in Jos. The founder  because of his believe in human capital development, being resourceful and useful to society, as well as deliberately exposing individuals, groups to contents that will result in the orientation and reorientation of the Nigerian and African people (Youths especially) to take their destiny in their hands and make their lives and society better instead of complaining endlessly over the failures and corruption in the system (religious, public, private, even Non-Governmental Organizations) was inspired to start Zemyen Studios to be part of the solution against backward, debilitating orientation and the challenge of teaming youth unemployment in the country and the African continent.
                     </p>
-                    <button className=" bg-[#4169e1] h-8 w-32 rounded mt-6 text-slate-50">BOOK NOW</button>
+                   <Link to="/Contact"><button className=" bg-[#4169e1] h-8 w-32 rounded mt-6 text-slate-50">BOOK NOW</button></Link> 
                 </section>
             </div>
         </div>
