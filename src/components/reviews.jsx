e9c96750bb0482430dbd8fc9808d75945772a2a0
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-12-05 07:36:52 +0100

    about

--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -3,7 +3,7 @@ import rev from "../images/zem7.jpg"
 const Reviews = () => {
     return ( 
         <>
-        <div className="w-[20%] flex flex-col justify-center rounded-[10px] items-center bg-[#3B6346]">
+        <div className="w-[20%] flex flex-col justify-center rounded-[10px] items-center bg-[#B49341]">
             <img className="h-[150px] rounded-t-[10px] brightness-50 w-[100%]"  src={rev} alt="" srcset="" />
             <div className="h-[150px]">
                 <h1>Reviews</h1>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -15,11 +15,14 @@ const LandingPage = () => {
                     <Navbar/>
                 </div>
                 <div className="w-[100%] h-[100%] left-0 right-0 top-0 bottom-0 pb-[23px]">
-                    <img className="h-[550px] brightness-50 w-full" width={'45%'} src={bag} alt="" srcset="" />
+                    <img className="h-[550px] brightness-50 w-full"  src={bag} alt="" srcset="" />
                 </div>
-                <div className="flex w-full">
-                    <div className="flex flex-col justify-between m-5 items-start">
+                <div className="flex w-full bg-[#3B6346] p-10">
+                    <div className="flex w-[100%] rounded-[10px] p-6 bg-white justify-around  items-start">
                         <div className="bg-[#B49341] rounded-[10px] cursor-pointer w-[40%] p-[12px] mt-3">
+                            <h1>hello</h1>
+                        </div>
+                        {/* <div className="bg-[#B49341] rounded-[10px] cursor-pointer w-[40%] p-[12px] mt-3">
                             <h1>hello</h1>
                         </div>
                         <div className="bg-[#B49341] rounded-[10px] cursor-pointer w-[40%] p-[12px] mt-3">
@@ -33,13 +36,10 @@ const LandingPage = () => {
                         </div>
                         <div className="bg-[#B49341] rounded-[10px] cursor-pointer w-[40%] p-[12px] mt-3">
                             <h1>hello</h1>
-                        </div>
-                        <div className="bg-[#B49341] rounded-[10px] cursor-pointer w-[40%] p-[12px] mt-3">
-                            <h1>hello</h1>
-                        </div>
-                    </div>
+                        </div> */}
                     <div className="">
-                        <img className="h-[550px] brightness-50 w-[]" src={rev} alt="" srcset="" />
+                        <img className="h-[350px] brightness-50 w-[300px]" src={rev} alt="" srcset="" />
+                    </div>
                     </div>
                 </div>
                 <div className="flex justify-around mt-10">
@@ -48,7 +48,7 @@ const LandingPage = () => {
                     <Reviews/>
                     <Reviews/>
                 </div>
-                <footer className="bg-[#B49341] h-[30vh] mt-10">
+                <footer className="bg-[#3B6346] h-[30vh] mt-10">
                     zzzzzzzzzzzzzzz
                 </footer>
             </div>
